f5e337015955ad329f1651fdb51444859ca56cca
eminaliyevx <eminaliyevx@gmail.com>
2023-08-19 06:04:57 +0400

    ✨ Complete portfolio page

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -1,3 +1,4 @@
 export * from "./education";
 export * from "./experience";
+export * from "./project";
 export * from "./skill";
---
+++ src/interfaces/project.ts
@@ -0,0 +1,17 @@
+import type { EntryFieldTypes } from "contentful";
+import type { ISkill } from "./skill";
+
+export interface IProject {
+  contentTypeId: "project";
+  fields: {
+    name: EntryFieldTypes.Text;
+    description: EntryFieldTypes.Text;
+    logo: EntryFieldTypes.AssetLink;
+    githubLink: EntryFieldTypes.Text;
+    externalLink: EntryFieldTypes.Text;
+    skills: EntryFieldTypes.Array<
+      EntryFieldTypes.EntryLink<ISkill<"technical">>
+    >;
+    images: EntryFieldTypes.Array<EntryFieldTypes.AssetLink>;
+  };
+}
